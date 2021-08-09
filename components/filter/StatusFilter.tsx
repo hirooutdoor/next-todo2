a94227f8d19dacaedeb0e207ce44dbaa1d1e5b0e
hiro <>
2021-08-10 06:44:58 +0900

    feat: Add classnames library and click event

--- components/filter/StatusFilter.tsx
+++ components/filter/StatusFilter.tsx
@@ -1,21 +1,46 @@
 import styles from 'styles/Home.module.css'
+import classnames from 'classnames'
 
 type Props = {
-  handleFilter: () => void
+  handleFilter: (stateFilter: string) => void
+  stateFilter: string
 }
 
 export const StatusFilter: React.FC<Props> = (props) => {
-  
-  const {handleFilter} = props;
+  const { handleFilter, stateFilter } = props
 
   return (
     <>
       <div className={styles.stateSort_area}>
-        <p className={styles.is_active} onClick={handleFilter}>All</p>
-        <p onClick={handleFilter}>Not Yet</p>
-        <p onClick={handleFilter}>In Progress</p>
-        <p onClick={handleFilter}>Done</p>
-  </div>
-  </>
+        <p
+          className={classnames({ [styles.is_active]: stateFilter === 'All' })}
+          onClick={() => handleFilter('All')}
+        >
+          All
+        </p>
+        <p
+          className={classnames({
+            [styles.is_active]: stateFilter === 'Not Yet',
+          })}
+          onClick={() => handleFilter('Not Yet')}
+        >
+          Not Yet
+        </p>
+        <p
+          className={classnames({
+            [styles.is_active]: stateFilter === 'In Progress',
+          })}
+          onClick={() => handleFilter('In Progress')}
+        >
+          In Progress
+        </p>
+        <p
+          className={classnames({ [styles.is_active]: stateFilter === 'Done' })}
+          onClick={() => handleFilter('Done')}
+        >
+          Done
+        </p>
+      </div>
+    </>
   )
-};
+}
