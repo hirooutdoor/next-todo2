68c07e7cc22a04d9f53950bb825868ffcb6807ec
hiro <hmaeyama0510@gmail.com>
2021-08-24 20:18:31 +0900

    fix:  not default import for bodyTheme #14

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import { ColorModeScript } from "@chakra-ui/react"
 import NextDocument, { Html, Head, Main, NextScript } from "next/document"
-import bodyTheme from "src/theme/theme"
+import { bodyTheme } from "src/theme/theme"
 
 export default class Document extends NextDocument {
 
